2623005ee68d63b30b395afeb8e45cb296d87918
hyunjin <s2113@e-mirim.hs.kr>
2023-05-10 12:56:13 +0900

    초기 커밋.

---
+++ ConsoleApp1/FileClass.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class FileClass
+    {
+        static void Main(string[] args)
+        {
+            OtherClass oc = new OtherClass();
+
+            Console.WriteLine("Hello World");
+        }
+
+        class ThridClass
+        {
+
+        }
+    }
+    internal class OtherClass
+    {
+        public OtherClass()
+        {
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program234 //클래스 파일명과 실제 이름이 달라도 됨 하지만 맞추는 걸 추천
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
